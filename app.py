db72b1e853deb181675e95304db252a1be32b7d2
danielkpodo <kpododanielnarh@gmail.com>
2020-07-30 20:17:12 +0000

    set-up pages for sections

--- app.py
+++ app.py
@@ -5,7 +5,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/works")
+def works():
+    return render_template("works.html")
 
 
 if __name__ == "__main__":
