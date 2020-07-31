488a621fdc8663a024dd230ca70d91c885d24657
danielkpodo <kpododanielnarh@gmail.com>
2020-07-31 00:16:19 +0000

    generated requirements.txt

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import csv
 
 app = Flask(__name__)
 
@@ -21,6 +22,26 @@ def contact():
 @app.route("/works")
 def works():
     return render_template("works.html")
+
+
+def write_to_csv(data):
+    with open("db.csv", mode="a", newline='') as file_object:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(
+            file_object, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route("/submit-form", methods=["POST"])
+def submit_form():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return render_template('thankyou.html')
+    else:
+        return "Something went wrong, try again!"
 
 
 if __name__ == "__main__":
